f87d8ff0e9b02c871dbfd2f55c7b732469aa02da
arseniyprogrammer <57635748+arseniyprogrammer@users.noreply.github.com>
2020-12-24 20:48:37 +0300

    Magic Ball

--- system/app.js
+++ system/app.js
@@ -1,4 +1,3 @@
-console.log("Магический шар")
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
       .then(() => navigator.serviceWorker.ready.then((worker) => {
